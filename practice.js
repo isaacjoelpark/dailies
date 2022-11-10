5074ca4da6e98aef94917c3ed4ea88a8f0b3057f
Isaac Park <isaac.joel.park@gmail.com>
2022-11-09 16:36:50 -0800

    add practice.js

---
+++ practice.js
@@ -0,0 +1,23 @@
+function square(n){
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < n; j++){
+            console.log(i*j);
+
+        }
+    }
+}
+
+square(10);
+let arr = [1,2,3,4,5,6,7,8,];
+let start = 0;
+let end = arr.length - 1;
+let target = 8;
+function binarySearch(arr, start, end, target){
+    if(start > end ) return false;
+    let midIndex = Math.floor(start + end /2);
+    if(arr[midIndex] ===target) return true;
+    
+    if(arr[midIndex] > target) return binarySearch(arr, start, midIndex -1, target);
+    else return binarySearch(arr, midIndex + 1, end, target);
+}
+console.log(arr, start, end, target)
