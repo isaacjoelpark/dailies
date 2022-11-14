36157effc2ce1da13caa37cbe348cac4b2bded97
Isaac Park <isaac.joel.park@gmail.com>
2022-11-13 23:04:48 -0800

    add index.js

--- index.js
+++ index.js
@@ -50,3 +50,8 @@ ReactDOM.render(
     <MainContent />
     document.getElementById("root")
 )
+
+const h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way to program"
+h1.className = "header"
+document.getElementById("root").append(h1)
