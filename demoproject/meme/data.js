23b9f5babb11f72f19b6df3d29e65ed856bfea6e
Isaac Park <isaac.joel.park@gmail.com>
2022-11-25 08:05:42 -0800

    add meme

---
+++ demoproject/meme/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        id: 1,
+        title: "Mount Fuji ",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3775 meters. Mount Fuji is the single most popular tourist site in japan, for both japanese and foreign tourists",
+        date: "12 Jan, 2021 - 24 Jan, 2021",
+        coverImg: "katie-zaferes.png",
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn Wedding Photography",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 125,
+        coverImg: "wedding-photography.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    }
+]
---
+++ meme/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from './Header'
+import Meme from "./Meme"
+/**
+ * Challenge: Build the Header component
+ */
+
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
