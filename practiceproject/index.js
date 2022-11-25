846cb2aca88965c44d312ee482d3f3fb087d6241
Isaac Park <isaac.joel.park@gmail.com>
2022-11-25 13:22:39 -0800

    add index.js

--- practiceproject/index.js
+++ practiceproject/index.js
@@ -1,5 +1,25 @@
-import React from "react"
-import ReactDOM form "react-dom"
-import App from "./App"
+// import React from "react"
+// import ReactDOM form "react-dom"
+// import App from "./App"
 
-ReactDOM.render(<App />,document.getElementById("root"))
+// ReactDOM.render(<App />,document.getElementById("root"))
+import React from "react"
+export default function App(){
+    const [count, setCount] = React.useState(0)
+}
+    function subtract(){
+        setCount(prevCount => prevcount - 1)
+    }
+
+    function addition(){
+        setCount(prevCount => prevCount + 1)
+    }
+    return(
+        <div className="counter">
+            <button className="counter--minus" onClick={subtract}>-</button>
+            <div className="counter-count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={addition}>+</button>
+        </div>
+    )
