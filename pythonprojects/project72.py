8aa4f815013e6915af1da2e0bdd1b50b0c515e54
Isaac Park <isaac.joel.park@gmail.com>
2022-11-28 14:51:31 -0800

    add work4

---
+++ pythonprojects/project72.py
@@ -0,0 +1,37 @@
+import random
+try:
+    import pyperclip # pyperclip copies text to the clipboard.
+except ImportError:
+    pass # If pyperclip is not installed, do nothing. It's no big deal.
+def main():
+"""Run the Spongetext program."""
+    print('''sPoNgEcAsE, bY aL sWeIGaRt Al@iNvEnTwItHpYtHoN.cOm
+eNtEr YoUr MeSsAgE:''')
+    spongetext = englishToSpongecase(input('> '))
+    print()
+    print(spongetext)
+    try:
+        pyperclip.copy(spongetext)
+        print('(cOpIed SpOnGeTexT to ClIpbOaRd.)')
+    except:
+        pass # Do nothing if pyperclip wasn't installed.
+def englishToSpongecase(message):
+"""Return the spongetext form of the given string."""
+    spongetext = ''
+    useUpper = False
+    for character in message:
+        if not character.isalpha():
+            spongetext += character
+            continue
+        
+        if useUpper:
+            spongetext += character.upper()
+        else:
+            spongetext += character.lower()
+# Flip the case, 90% of the time.
+        if random.randint(1, 100) <= 90:
+            useUpper = not useUpper # Flip the case.
+    return spongetext
+# If this program was run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
